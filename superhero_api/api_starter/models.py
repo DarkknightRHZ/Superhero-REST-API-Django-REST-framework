7381aa77d50b2256ae89fd561b8ef90ba9271221
Mohammed Raihan Hussain <rhzinuk@gmail.com>
2020-07-18 23:25:10 +0200

    Superhero model created, basic setup done

--- superhero_api/api_starter/models.py
+++ superhero_api/api_starter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Superhero(models.Model):
+    hero_id = models.IntegerField(unique=True, null=False)
+    name = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    universe = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
