b0c78743692f62873c978cbfd4d41a7b77de2a7f
Mohammed Raihan Hussain <rhzinuk@gmail.com>
2020-07-19 01:27:37 +0200

    GET, PUT, DELETE methods added

--- superhero_api/api_starter/urls.py
+++ superhero_api/api_starter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import superhero_list
+from .views import superhero_list, superhero_detail
 
 urlpatterns = [
     path('superhero/', superhero_list),
+    path('superhero_detail/<int:id>/', superhero_detail)
 ]
--- superhero_api/api_starter/views.py
+++ superhero_api/api_starter/views.py
@@ -19,3 +19,27 @@ def superhero_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def superhero_detail(request, id):
+    try:
+        superhero = Superhero.objects.get(hero_id=id)
+
+    except Superhero.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = Superhero_serializer(superhero)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = Superhero_serializer(superhero, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        superhero.delete()
+        return HttpResponse(status=204)
+
